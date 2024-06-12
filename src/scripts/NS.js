e3f8e7d88f24b0f03ebf104a2a0505669f1f4bbb
Thibaut <59665309+Cyber-Thibaut@users.noreply.github.com>
2024-06-13 01:14:31 +0200

    mise à jour pour la navette spéciale

---
+++ src/scripts/NS.js
@@ -0,0 +1,79 @@
+const now = new Date();
+const weekday = now.getDay(); // 0 (Dimanche) à 6 (Samedi)
+const hour = now.getHours();
+const minute = now.getMinutes();
+const date = formatDate(now); // Convertir la date en chaîne "YYYY-MM-DD"
+const today = new Date();
+
+const vacationDates = [
+    { start: new Date("2023-07-01"), end: new Date("2023-09-03") }, // Période de vacances en juillet-septembre 2023
+    { start: new Date("2023-10-21"), end: new Date("2023-11-05") }, // Période de vacances en octobre-novembre 2023
+    { start: new Date("2023-12-23"), end: new Date("2024-01-07") }, // Période de vacances en décembre-janvier 2024
+    { start: new Date("2024-05-22"), end: new Date("2024-02-23") }, // Période de vacances en mai 2024 
+    { start: new Date("2024-02-17"), end: new Date("2024-03-03") }, // Période de vacances en février-mars 2024
+    { start: new Date("2024-04-13"), end: new Date("2024-04-28") }, // Période de vacances en avril 2024
+    { start: new Date("2024-07-06"), end: new Date("2024-09-01") }  // Période de vacances en juillet-septembre 2024
+    // Ajouter d'autres périodes de vacances si nécessaire
+];
+
+
+function isDateInVacationRanges(date, ranges) {
+    for (const range of ranges) {
+        const startDate = range.start;
+        const endDate = range.end;
+        if (date >= startDate && date <= endDate) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const isVacation = isDateInVacationRanges(today, vacationDates);
+
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
+const holidayDates = [
+    "2023-11-01",
+    "2023-11-11",
+    "2023-12-25",
+    "2024-01-01",
+    "2024-04-01",
+    "2024-05-08",
+    "2024-05-09",
+    "2024-05-20",
+    "2024-07-14",
+    "2024-08-15"
+];
+// Exemple de dates de jours fériés
+const premierMai = new Date(today.getFullYear(), 4, 1);
+const estFerie = holidayDates.includes(date);
+const estPremierMai = today.getTime() === premierMai.getTime();
+
+function updateBusTimes() {
+    const nextBus = `<div role="alert" class="alert alert-warning">
+    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+    </svg>
+    <span>Regardez les infos trafic pour savoir si la ligne est en fonctionnement.</span>
+</div>`;
+    document.getElementById("nextBusTime").innerHTML = nextBus;
+}
+setInterval(updateBusTimes, 30000); // Mettez 30000 pour actualiser toutes les 30 secondes
+updateBusTimes(); // Appel initial pour mettre à jour les données immédiatement
+
+// Update current time every second
+function updateCurrentTime() {
+    const currentTimeElement = document.getElementById('current-time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    currentTimeElement.textContent = `Mis à jour à : ${hours}:${minutes}`;
+    document.getElementById('destination').innerHTML = ``;
+
+}
+setInterval(updateCurrentTime, 1000);
+updateCurrentTime(); // Update initially

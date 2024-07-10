a6a6bb1602120b600d08779804b65a6ff83baf8f
Thibaut <59665309+Cyber-Thibaut@users.noreply.github.com>
2024-07-10 11:04:29 +0200

    validation ajout des JO

--- src/scripts/902.js
+++ src/scripts/902.js
@@ -106,7 +106,7 @@ function updateCurrentTime() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
-    currentTimeElement.textContent = `Mis à jour à : ${hours}:${minutes}`;
+    currentTimeElement.textContent = `Dernière mise à jour : ${hours}:${minutes}`;
     document.getElementById('destination').innerHTML = `<h2 style='font-size: 40px;'>Direction Observatoire</h2><br>`;
 
 }
